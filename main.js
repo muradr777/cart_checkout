de9d1022be40d69d635edcc7deb24b52d2c1e2ed
Murad <muradrustamov@gmail.com>
2019-03-13 18:39:02 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+var btnSelectTrigger = $(".btn-select-trigger");
+btnSelectTrigger.on("click", function() {
+  var total,
+    prices = [],
+    ids = [];
+  $(this).toggleClass("active");
+
+  btnSelectTrigger.each(function(i, e) {
+    if ($(e).hasClass("active")) {
+      prices.push(
+        +$(e)
+          .parent()
+          .siblings(".product-price-block")
+          .find("span")
+          .text()
+          .trim()
+      );
+      ids.push($(e).data("id"));
+    }
+
+    if (prices.length > 0) {
+      total = Math.floor(prices.reduce(getSum) * 100) / 100;
+      $("#total-price-counter").text("$" + total);
+    } else {
+      $("#total-price-counter").text("$0");
+    }
+
+    if (ids.length > 0) {
+      $("#selected-prod-ids").val(ids.reduce(getIds));
+    } else {
+      $("#selected-prod-ids").val("");
+    }
+  });
+});
+
+function getSum(total, num) {
+  return total + num;
+}
+
+function getIds(total, id) {
+  return total + "," + id;
+}
